328aeff357398e10ff8da936bd2d12fa14e639eb
IndianBartonka <bartonka2017@outlook.com>
2023-08-16 13:07:18 +0200

    init chat pack

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@ export const mcprefix = "§7[§aBDS§1 Auto Enable §a]§r ";
 export const consoleprefix = "[BDS Auto Enable] ";
 
 
-
 //world.afterEvents.entityDie.subscribe(
 //  ({ deadEntity: player, damageSource: { cause , damagingEntity} }) => {
 //    const name = player.name;
@@ -26,18 +25,13 @@ export const consoleprefix = "[BDS Auto Enable] ";
 const cooldowns = new Map();
 world.beforeEvents.chatSend.subscribe((data) => {
     if (cooldowns.has(data.sender.name) && (Date.now() - cooldowns.get(data.sender.name)) / 1000 < 2) {
-      data.sender.sendMessage("§cZwolnij troche! (2s)");
+      data.sender.sendMessage(mcprefix + "§cZwolnij troche! (2s)");
       data.cancel = true;
     } else {
       cooldowns.set(data.sender.name, Date.now());
       const name = data.sender.name;
       const message = data.message;
-      if(message.startsWith("!tps")){
-          getTps(false);
-          data.cancel = true;
-      }
-      console.log("[Chat] PlayerChat:"+ name + " Message:" + message)
-      system.run(() => sendRequest(chatMessage(name, message)));
+      console.log("[Chat] PlayerChat:"+ name + " Message:" + message);
     }
   },
 );
