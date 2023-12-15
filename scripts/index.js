f2c0a247e34631d7fd008e85bf84a5cb86dafa2c
IndianBartonka <bartonka2017@outlook.com>
2023-12-15 08:12:44 +0100

    Better cooldown

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,28 @@ export const consoleprefix = "[BDS Auto Enable] ";
 
 world.afterEvents.playerSpawn.subscribe(
   ({ player, initialSpawn }) => {
-      if (!initialSpawn) return;
-      console.log("PlayerJoin:" + player.name);
+    if (!initialSpawn) return;
+    console.log("PlayerJoin:" + player.name);
   },
 );
 
 world.afterEvents.entityDie.subscribe(
-  ({ deadEntity: player, damageSource: { cause , damagingEntity} }) => {
+  ({ deadEntity: player, damageSource: { cause, damagingEntity } }) => {
     const name = player.name;
     let damageRR;
 
-    if(cause == EntityDamageCause.entityAttack){
+    if (cause == EntityDamageCause.entityAttack) {
       damageRR = damagingEntity?.typeId;
-      if(damagingEntity instanceof Player ){
+      if (damagingEntity instanceof Player) {
         damageRR = damagingEntity.name;
       }
     } else {
       damageRR = cause;
     }
-    console.log("PlayerDeath:" + name +" Casue:" + damageRR );
+    console.log("PlayerDeath:" + name + " Casue:" + damageRR);
 
   },
-  { entityTypes: [ "minecraft:player" ] },
+  { entityTypes: ["minecraft:player"] },
 );
 //TODO: Dodać wsparcja dla 
 // PlayerBreakBlockBeforeEvent (https://learn.microsoft.com/en-us/minecraft/creator/scriptapi/minecraft/server/playerbreakblockbeforeevent)
@@ -37,36 +37,36 @@ world.afterEvents.entityDie.subscribe(
 
 const cooldowns = new Map();
 world.beforeEvents.chatSend.subscribe((data) => {
-      const player = data.sender;
-      const name = player.name;
-      const message = data.message;
+  const player = data.sender;
+  const name = player.name;
+  const message = data.message;
 
-    if (cooldowns.has(name) && (Date.now() - cooldowns.get(name)) / 1000 < 2) {
-      data.sender.sendMessage(mcprefix + "§cZwolnij troche! (2s)");
+  if (cooldowns.has(name) && (Date.now() - cooldowns.get(name)) / 1000 < 2) {
+    data.sender.sendMessage(mcprefix + "§cZwolnij troche! (2s)");
+    data.cancel = true;
+  } else {
+    if (message.startsWith("!tps")) {
+      getTps();
       data.cancel = true;
-    } else {
-      if(!player.isOp()){
-      cooldowns.set(name, Date.now());
-      }
- 
-      if(message.startsWith("!tps")){
-        getTps();
-        data.cancel = true; 
-        return;
-      }
-
-      if(message.startsWith("!")){
-        console.log("PlayerCommand:" + name + " Command:" + message + " Op:" + player.isOp());
-        data.cancel = true; 
-        return;
-      }
-
-      console.log("PlayerChat:"+ name + " Message:" + message);
-      data.cancel = appHandledMessages; 
+      return;
     }
-  },
+
+    if (message.startsWith("!")) {
+      console.log("PlayerCommand:" + name + " Command:" + message + " Op:" + player.isOp());
+      data.cancel = true;
+      return;
+    }
+
+    if (!player.isOp()) {
+      cooldowns.set(name, Date.now());
+    }
+
+    console.log("PlayerChat:" + name + " Message:" + message);
+    data.cancel = appHandledMessages;
+  }
+},
 );
 
 
-world.sendMessage(mcprefix +  '§3Wczytano!');
+world.sendMessage(mcprefix + '§3Wczytano!');
 console.log(consoleprefix + 'Wczytano!');
