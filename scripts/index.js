1e21130bb8900e1499ef8e5866fff6c92164ccfa
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-02-22 10:29:52 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,12 @@
 import { world, system, EntityDamageCause, Player } from "@minecraft/server";
 import "./tps.js";
 import "./event/death.js";
+import "./event/player.js";
 import { getTps } from "./tps.js";
 
 const appHandledMessages = false;
 export const mcprefix = "§7[§aBDS§1 Auto Enable §a]§r ";
 export const consoleprefix = "[BDS Auto Enable] ";
-
-world.afterEvents.playerSpawn.subscribe(
-  ({ player, initialSpawn }) => {
-    if (initialSpawn) console.log("PlayerJoin:" + player.name);
-    console.log("PlayerSpawn:" + player.name);
-  },
-);
-
-world.afterEvents.playerDimensionChange.subscribe(({ player, fromDimension, fromLocation, toDimension, toLocation }) => {
-  // console.log("Z wymiaru: " + fromDimension.id);
-  // console.log("Na wymiar: " + toDimension.id);
-  // console.log("Lokacja przed zmianą wymiaru: " + fromLocation.x + " " + fromLocation.y + " " + fromLocation.z);
-  // console.log("Lokacja po zmianie wymiaru: " + toLocation.x + " " + toLocation.y + " " + toLocation.z);
-
-  console.log("DimensionChangePlayer: " + player.name + " From:" + fromDimension.id + " To:" + toDimension.id)
-});
-
-world.beforeEvents.playerBreakBlock.subscribe((event) => {
-  const name = event.player.name;
-  const block = event.block;
-
-  const x = block.location.x.toFixed(2);
-  const y = block.location.y.toFixed(2);
-  const z = block.location.z.toFixed(2);
-
-  const position = "X: " + x + " Y:" + y + " Z:" + z;
-
-
-  console.log("PlayerBreakBlock:" + name + " Block:" + block.typeId + " Position:" + position);
-});
-
-world.afterEvents.playerPlaceBlock.subscribe((event) => {
-  const name = event.player.name;
-  const block = event.block;
-
-  const x = block.location.x.toFixed(2);
-  const y = block.location.y.toFixed(2);
-  const z = block.location.z.toFixed(2);
-
-  const position = "X: " + x + " Y:" + y + " Z:" + z;
-
-
-  console.log("PlayerPlaceBlock:" + name + " Block:" + block.typeId + " Position:" + position);
-});
 
 system.afterEvents.scriptEventReceive.subscribe((event) => {
   // console.log(
@@ -63,90 +20,6 @@ system.afterEvents.scriptEventReceive.subscribe((event) => {
   }
 
 });
-
-/**
- * Używam §lCOS§l§r aby w BDS-Auto-Enable podmienic §l na ** i wiadomość na discord byłą wybrubiona
- */
-
-world.afterEvents.entityDie.subscribe(
-  ({ deadEntity: player, damageSource: { cause, damagingEntity, damagingProjectile } }) => {
-    const name = player.name;
-    let deathMessage;
-
-    switch (cause) {
-      case EntityDamageCause.entityAttack:
-        if (damagingEntity instanceof Player) {
-          deathMessage = "zabity przez §l" + damagingEntity.name + "§l";
-        } else {
-          deathMessage = "zabity przez §l" + damagingEntity?.typeId + "§l";
-        }
-        break;
-
-      case EntityDamageCause.entityExplosion:
-      case EntityDamageCause.blockExplosio:
-        deathMessage = "wysadzony w powietrze przez §l" + damagingEntity?.typeId + "§l";
-        break;
-
-      case EntityDamageCause.projectile:
-        if (damagingEntity instanceof Player) {
-          deathMessage = "zastrzelony przez §l" + damagingEntity.name + "§l§r przy użyciu §l" + damagingProjectile?.typeId + "§l";
-        } else {
-          deathMessage = "zastrzelony przez §l" + damagingEntity?.typeId + "§l§r przy użyciu §l" + damagingProjectile?.typeId + "§l";
-        }
-
-
-        break;
-
-      case EntityDamageCause.fall:
-        deathMessage = "§lspadł z wysokości§l";
-        break;
-
-      case EntityDamageCause.drowning:
-        deathMessage = "§lutonoł§l"
-        break;
-
-      case EntityDamageCause.fireTick:
-        deathMessage = "§lstanoł w płomieniach§l"
-        break;
-
-      case EntityDamageCause.suffocation:
-        deathMessage = "§lzadusił się w ścianie§l"
-        break;
-
-      case EntityDamageCause.flyIntoWall:
-        deathMessage = "§lwleciał w ściane§l"
-        break;
-
-      case EntityDamageCause.anvil:
-        deathMessage = "został zgnieciony przez spadające §lkowadło§l"
-        break;
-
-      case EntityDamageCause.freezing:
-        deathMessage = "§lzamarzł§l"
-        break;
-
-      case EntityDamageCause.fireworks:
-        deathMessage = "§leksplodował z hukiem§l"
-        break;
-
-      case EntityDamageCause.stalactite:
-        deathMessage = "został przebity przez spadający §lstalaktyt§l"
-        break;
-
-      case EntityDamageCause.suicide:
-        deathMessage = "popełnił §lsamobójstwo§l"
-        break;
-
-      default:
-        deathMessage = "zabity przez §l" + cause + "§l";
-        break;
-    }
-
-    console.log("PlayerDeath:" + name + " DeathMessage:" + deathMessage);
-  },
-  { entityTypes: ["minecraft:player"] },
-);
-
 
 const cooldowns = new Map();
 world.beforeEvents.chatSend.subscribe((data) => {
@@ -173,7 +46,6 @@ world.beforeEvents.chatSend.subscribe((data) => {
   }
 },
 );
-
 
 world.sendMessage(mcprefix + '§3Wczytano!');
 console.log(consoleprefix + 'Wczytano!');
