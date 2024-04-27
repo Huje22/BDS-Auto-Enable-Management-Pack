7dd88997c3c9045fffcc06ffffd6556c6070a8da
IndianBartonka <bartonka2017@outlook.com>
2024-04-27 23:26:51 +0200

    Better TPS counting

--- scripts/oldtps.js
+++ scripts/oldtps.js
@@ -1,6 +1,7 @@
 import { world, system } from '@minecraft/server';
 import { mcprefix, consoleprefix } from './index.js';
 
+//Old ONE
 let tps = 0;
 let lastTime = Date.now();
 
@@ -29,3 +30,25 @@ export function getTps() {
 system.runInterval(() => getTps(), 3000);
 system.runInterval(() => getTps(), 2999);
 system.runInterval(() => getTps(), 2998);
+
+
+
+
+
+//Old Two
+
+function getCurrentTPS() {
+    const start = Date.now();
+    let tps = 0;
+    return new Promise((resolve) => {
+        const run = system.runInterval(() => {
+            if (Date.now() - start < 1000) {
+                tps += 1;
+            } else {
+                system.clearRun(run);
+                resolve(tps);
+            }
+        });
+    });
+}
+
--- scripts/tps.js
+++ scripts/tps.js
@@ -1,41 +1,37 @@
 import { world, system } from '@minecraft/server';
 import { mcprefix, consoleprefix } from './index.js';
 
-function getCurrentTPS() {
-    const start = Date.now();
-    let tps = 0;
-    return new Promise((resolve) => {
-        const run = system.runInterval(() => {
-            if (Date.now() - start < 1000) {
-                tps += 1;
-            } else {
-                system.clearRun(run);
-                resolve(tps);
-            }
-        });
-    });
+let lastTick = Date.now();
+const tickLengths = [];
+let tickTotals = 0;
+
+system.runInterval(() => tick());
+
+function tick() {
+    const now = Date.now();
+    const deltaTime = (now - lastTick) / 1000;
+    tickLengths.unshift(deltaTime);
+    tickTotals += deltaTime;
+    while (tickTotals > 1) {
+        tickTotals -= tickLengths.pop();
+    }
+
+    lastTick = now;
 }
 
 export function getTps() {
-    getCurrentTPS().then((tps) => {
-        //Narazie TPS są jako int ale kiedyś możliwe że będą jako double dlatego przystosowuje je pod to
-        let tpsMess = mcprefix + `TPS: `;
-        if (tps >= 17) {
-            tpsMess += `§2${tps.toFixed(2)}/20.00`;
-        } else if (tps >= 10) {
-            tpsMess += `§e${tps.toFixed(2)}/20.00`;
-        } else {
-            tpsMess += `§c${tps.toFixed(2)}/20.00`;
-        }
-        world.sendMessage(tpsMess);
-        console.log(consoleprefix + `TPS: ` + parseInt(tps));
-
-    }).catch((error) => {
-        world.sendMessage(mcprefix
-            + 'Nie udało się pozyskać ilości TPS servera' +
-            error);
-        console.error(`An error occurred: ${error}`);
-    });
+    const tps = tickLengths.length;
+    //Narazie TPS są jako int ale kiedyś możliwe że będą jako double dlatego przystosowuje je pod to
+    let tpsMess = mcprefix + `TPS: `;
+    if (tps >= 17) {
+        tpsMess += `§2${tps.toFixed(2)}/20.00`;
+    } else if (tps >= 10) {
+        tpsMess += `§e${tps.toFixed(2)}/20.00`;
+    } else {
+        tpsMess += `§c${tps.toFixed(2)}/20.00`;
+    }
+    world.sendMessage(tpsMess);
+    console.log(consoleprefix + `TPS: ` + parseInt(tps));
 }
 
 system.runInterval(() => { getTps() }, 20 * 150);
